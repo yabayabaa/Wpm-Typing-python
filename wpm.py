571f738bc1917ecc9a1d4bbb351b9eb82d3aee20
Yabayabaa <sherpanimasange877@gmail.com>
2024-07-08 13:57:52 +0545

    wpm is calculated

--- wpm.py
+++ wpm.py
@@ -1,34 +1,40 @@
 import curses
 from curses import wrapper
+import time
 
 def start_screen(stdscr):
-     # Clear the screen
     stdscr.clear()
-    
-    # Add a string at position (0, 0)
     stdscr.addstr("Welcome To The Typotest")
     stdscr.addstr("\nPress any keys to begin")
-    
-    # Refresh the screen to show the changes
     stdscr.refresh()
-
-    # Wait for the user to press a key
     stdscr.getkey()
 
+
+def display_text(stdscr, target, current,wpm=0):
+    stdscr.addstr(target)
+    stdscr.addstr(1,0,f"WPM :{wpm}")
+
+    for i,char in enumerate (current):
+        correct_char = target[i]
+        color = curses.color_pair(1)
+        if char != correct_char:
+            color = curses.color_pair(2)
+
+        stdscr.addstr(0, i, char, color )
 
 def wpm_test(stdscr):
     target_text = "Hello World This is a demo of typo\n"
     current_text = []
-
+    wpm = 0
+    start_time = time.time()
 
     while True:
+        time_elapsed = max(time.time() - start_time,1)
+        wpm = round((len(current_text)/(time_elapsed / 60))/5)
+
 
         stdscr.clear()
-        stdscr.addstr(target_text)
-
-        for char in current_text:
-            stdscr.addstr(char, curses.color_pair(1))
-
+        display_text(stdscr,target_text,current_text,wpm)
         stdscr.refresh()
 
         key = stdscr.getkey()
@@ -36,18 +42,20 @@ def wpm_test(stdscr):
         if ord(key)== 27:
             break
         if key in ("KEY_BACKSPACE", '\b', "\x7f"):
-            if len(current_text)>0:
+            if len(current_text)> 0:
                 current_text.pop()
 
-        else:
+        elif len(current_text)< len(target_text):
             current_text.append(key)
+
+
 
 
 
 def main(stdscr):
     curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
     curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
-    curses.init_pair(2, curses.COLOR_WHITE, curses.COLOR_BLACK)
+    curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_BLACK)
 
     start_screen(stdscr)
     wpm_test(stdscr)
